1cc29a1acbe2372128a2b19f6638cc1d11dab7b9
chinasemb <1032944759@qq.com>
2019-04-15 14:25:15 +0800

    4.15.14.25

---
+++ 4.15地图/js/index.js
@@ -0,0 +1,55 @@
+$('.region-button1').click(function () {
+    $('.map').toggle();
+    $('.region .region-button2').toggle();
+    $('.region .region-button3').toggle();
+    $('.region .region-button4').toggle();
+    $('.region .region-button5').toggle();
+    $('.region .region-button6').toggle();
+    $('.art1').toggle();
+    
+})
+$('.region-button2').click(function () {
+    $('.map').toggle();
+    $('.region .region-button1').toggle();
+    $('.region .region-button3').toggle();
+    $('.region .region-button4').toggle();
+    $('.region .region-button5').toggle();
+    $('.region .region-button6').toggle();
+    $('.art2').toggle();
+})
+$('.region-button3').click(function () {
+    $('.map').toggle();
+    $('.region .region-button2').toggle();
+    $('.region .region-button1').toggle();
+    $('.region .region-button4').toggle();
+    $('.region .region-button5').toggle();
+    $('.region .region-button6').toggle();
+    $('.art3').toggle();
+})
+$('.region-button4').click(function () {
+    $('.map').toggle();
+    $('.region .region-button2').toggle();
+    $('.region .region-button3').toggle();
+    $('.region .region-button1').toggle();
+    $('.region .region-button5').toggle();
+    $('.region .region-button6').toggle();
+    $('.art4').toggle();
+})
+$('.region-button5').click(function () {
+    $('.map').toggle();
+    $('.region .region-button2').toggle();
+    $('.region .region-button3').toggle();
+    $('.region .region-button4').toggle();
+    $('.region .region-button1').toggle();
+    $('.region .region-button6').toggle();
+    $('.art5').toggle();
+})
+$('.region-button6').click(function () {
+    $('.map').toggle();
+    $('.region .region-button2').toggle();
+    $('.region .region-button3').toggle();
+    $('.region .region-button4').toggle();
+    $('.region .region-button5').toggle();
+    $('.region .region-button1').toggle();
+    $('.art6').toggle();
+})
